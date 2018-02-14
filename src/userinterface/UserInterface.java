ae2f27620e0b06cc8e9ecfe2df8291bf3bca3232
Vlad <novlad26@gmail.com>
2018-02-15 01:02:08 +0300

    Wrote part of first group of elements

--- src/userinterface/UserInterface.java
+++ src/userinterface/UserInterface.java
@@ -6,12 +6,15 @@
 package userinterface;
 
 import javafx.application.Application;
+import javafx.geometry.*;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+//import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.collections.*;
 
 /**
  *
@@ -21,18 +24,58 @@ public class UserInterface extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Edit Button");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+        
+        /*--------делаем панель, разделённую на столбцы и строки-------------*/
+        GridPane root = new GridPane();
+        root.setPadding(new Insets(10,10,10,10));
+        root.setHgap(5);
+        root.setVgap(5);
+        
+        /*--------делаем выпадающий список-----------------------------------*/
+        ComboBox<String> comboBox = new ComboBox<String>();
+        ObservableList<String> listForComboBox = FXCollections.observableArrayList(
+                "low", "medium", "hight"
+        );
+        comboBox.setItems(listForComboBox);
+        comboBox.getSelectionModel().select(0);
+        GridPane.setConstraints(comboBox, 0, 1);
+        
+        /*--------делаем текстовое поле для ввода----------------------------*/
+        TextField firstTextField = new TextField();
+        GridPane.setConstraints(firstTextField, 0, 0);
+        //firstTextField.;
+        
+        /*--------делаем кнопку для копирования текста в ComboBox------------*/
+        Button button1 = new Button();
+        button1.setText("Edit Button");
+        button1.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                String newItem = firstTextField.getText();
+                int numOfItem = 0;
+                while(comboBox.getValue() != null){
+                    comboBox.getSelectionModel().select(numOfItem);
+                    if(comboBox.getValue() == null){
+                       comboBox.getItems().add(newItem);
+                       break;
+                    }
+                    if(comboBox.getValue() == newItem){
+                        //!!!!!!!!!!!!!!!!!!!!!!!!вывод сообщщения об ошибке!!!!!!!!!!!!!!!!!!!!!!!!!
+                        break;
+                    }
+                    numOfItem++;
+                }
+                
+                //System.out.println("Hello World!");
             }
         });
+        GridPane.setConstraints(button1, 1, 0);
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+
+        
+        root.getChildren().addAll(comboBox, button1, firstTextField);
         
         Scene scene = new Scene(root, 300, 250); //стандарт 300 на 250
         
